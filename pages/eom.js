cd800493be9265bc96ebfa21e192a572ea8c37c2
TyreseJin <tyresejin3915@gmail.com>
2022-02-02 11:42:53 -0300

    Setting eom page

---
+++ pages/eom.js
@@ -0,0 +1,36 @@
+import styles from "../styles/eom.module.css"
+
+export const EOM = ({employee}) => {
+    console.log(employee)
+
+    return (
+        <div className="page-container">
+            <div className={styles.main}>
+                <h1 className={styles.employeeOfTheMonth}>Employee of The Month</h1>
+
+                <div className={styles.employeeOfTheMonth}>
+                    <h3 >{employee.name}</h3>
+                    <h6>{employee.position}</h6>
+                    <img src={employee.image}/>
+                    <p>{employee.description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const getServerSideProps = async pageContext => {
+    const apiResponse = await fetch(
+        "https://my-json-server.typicode.com/portexe/next-news/employeeOfThEMonth"
+    )
+
+    const employee = await apiResponse.json()
+
+    return {
+        props: {
+            employee: employee
+        }
+    }
+}
+
+export default EOM
