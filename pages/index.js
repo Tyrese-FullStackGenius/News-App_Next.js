d5ff2b0118209fbe9382ed79a4cb7bc135c2a537
TyreseJin <tyresejin3915@gmail.com>
2022-02-07 18:09:11 -0300

    Adding reference video

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import {Toolbar} from "../components/toolbar"
 
@@ -11,6 +10,10 @@ export default function Home() {
         <h1>Next.js News App</h1>
 
         <h3>Your one stop shop for the latest news articles</h3>
+        <p>
+          Project based on the PortEXE video:
+          <a href="https://www.youtube.com/watch?v=xtItzwYG6oQ"> "Coding A News App With Next.js"</a>
+        </p>
       </div>
     </div>
   )
