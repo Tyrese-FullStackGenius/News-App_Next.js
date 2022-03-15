be92a5b0978d37bdc050238cd67009968930d108
TyreseJin <tyresejin3915@gmail.com>
2022-03-14 23:41:48 -0300

    Adding <meta/> tags, image for webpage preview and new page icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+          <title>NextJS News App</title>
+          <link rel="icon" href="../public/favicon.ico" />
+          <meta name="description" content="A simple NextJS application to get the recent news from News API - Brazil located."/>
+          <meta property="og:image" content="../public/news-icon.png"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
